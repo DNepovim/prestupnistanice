97f20ae3aba70a13ba6b11c847d2c1ae1934969c
Dominik Bláha <jsem@dominikblaha.cz>
2025-10-05 06:08:10 +0200

    Books categories

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,24 @@
-
 const SLUGS = {
   books: "knihy",
   auhtors: "autori",
 };
+
+export const CATEGORIES = {
+  forKids: { name: "Pro děti", slug: "pro-deti" },
+  philosophy: { name: "Pro filosofy", slug: "pro-filosofy" },
+  novel: { name: "Pro romantiky", slug: "pro-romantiky" },
+  poetry: { name: "Pro poéty", slug: "pro-poety" },
+} as const;
+
+export type CategoryKey = keyof typeof CATEGORIES;
+export type CategorySlug = (typeof CATEGORIES)[CategoryKey]["slug"];
+
+export const categoriesSlugToKey = {
+  "pro-deti": "forKids",
+  "pro-filosofy": "philosophy",
+  "pro-romantiky": "novel",
+  "pro-poety": "poetry",
+} as const;
 
 export const routes = {
   homepage: () => "/",
@@ -10,6 +26,8 @@ export const routes = {
     list: () => `/${SLUGS.books}`,
     detail: (book: { _sys: { filename: string } }) =>
       `/${SLUGS.books}/${book._sys.filename}`,
+    category: (key: keyof typeof CATEGORIES) =>
+      `/${SLUGS.books}/${CATEGORIES[key].slug}`,
   },
   authors: {
     detail: (author: { _sys: { filename: string } }) =>
