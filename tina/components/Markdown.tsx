b4fa2d4045ec6e23fc3031f7edc9c6050e98b61d
Dominik Bláha <jsem@dominikblaha.cz>
2025-10-15 10:58:36 +0200

    Narrow types in markdown

--- tina/components/Markdown.tsx
+++ tina/components/Markdown.tsx
@@ -1,17 +1,17 @@
 import MarkdownEditor, { commands } from '@uiw/react-md-editor'
+import { isString } from 'narrowland'
 import { wrapFieldsWithMeta } from 'tinacms'
 
-export const MarkdownInput = wrapFieldsWithMeta<string>(({ input }) => {
+export const MarkdownInput = wrapFieldsWithMeta(({ input }) => {
   return (
     <div data-color-mode="light">
       <div className="wmde-markdown-var"> </div>
 
       <MarkdownEditor
-        value={input.value ?? ''}
+        value={isString(input.value) ? input.value : ''}
         preview="edit"
         height="100%"
         visibleDragbar={false}
-        
         commands={[
           commands.heading,
           commands.bold,
