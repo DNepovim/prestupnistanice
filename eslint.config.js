254b916ebdcb73d74eec53e1c1af5277102b91a8
Dominik Bláha <jsem@dominikblaha.cz>
2025-11-28 13:14:50 +0100

    Fix ts and eslint

--- eslint.config.js
+++ eslint.config.js
@@ -61,7 +61,7 @@ export default defineConfig(
       parser: svelteParser,
       parserOptions: {
         parser: tseslint.parser,
-        project: true,
+        project: ['./tsconfig.eslint.json'],
         tsconfigRootDir: import.meta.dirname,
         extraFileExtensions: ['.svelte'],
       },
